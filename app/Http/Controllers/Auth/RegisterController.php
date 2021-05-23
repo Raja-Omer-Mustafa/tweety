32d46572e8b34469c22fd348446a88784e63a25d
Raja-Omer-Mustafa <rajaomermustafa@gmail.com>
2021-05-23 14:35:13 +0500

    password validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,16 @@ class RegisterController extends Controller
                 'unique:users',
                env('EMAIL_PATTERN'),
             ],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => [
+                'required', 
+                'string',
+                'min:8',
+                'regex:/[a-z]/',      // must contain at least one lowercase letter
+                'regex:/[A-Z]/',      // must contain at least one uppercase letter
+                'regex:/[0-9]/',      // must contain at least one digit
+                'regex:/[@$!%*#?&]/', // must contain a special character
+                'confirmed'
+            ],
         ]);
     }
 
