fca97f015ee27d589ba31b8423230ed727e51dbb
Raja-Omer-Mustafa <rajaomermustafa@gmail.com>
2021-05-26 14:24:20 +0500

    fix password issue

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -91,7 +91,7 @@ class RegisterController extends Controller
             'username' => $data['username'],
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => $data['password'],
         ]);
     }
 }
